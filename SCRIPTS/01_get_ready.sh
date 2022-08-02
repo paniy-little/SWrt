3e06636326c34e130dc4c22a6e935205f4bad034
paniy <louisowen@me.com>
2022-08-02 17:06:35 +0800

    .

---
+++ SCRIPTS/01_get_ready.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+latest_release="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][0-9]/p' | sed -n 1p | sed 's/.tar.gz//g')"
+git clone --single-branch -b ${latest_release} https://github.com/openwrt/openwrt openwrt_release
+git clone --single-branch -b openwrt-22.03 https://github.com/openwrt/openwrt openwrt
+rm -f ./openwrt/include/version.mk
+rm -f ./openwrt/include/kernel.mk
+rm -f ./openwrt/include/kernel-5.10
+rm -f ./openwrt/include/kernel-version.mk
+rm -f ./openwrt/include/toolchain-build.mk
+rm -f ./openwrt/include/kernel-defaults.mk
+rm -f ./openwrt/package/base-files/image-config.in
+rm -rf ./openwrt/target/linux/*
+rm -rf ./openwrt/package/kernel/linux/*
+cp -f ./openwrt_release/include/version.mk ./openwrt/include/version.mk
+cp -f ./openwrt_release/include/kernel.mk ./openwrt/include/kernel.mk
+cp -f ./openwrt_release/include/kernel-5.10 ./openwrt/include/kernel-5.10
+cp -f ./openwrt_release/include/kernel-version.mk ./openwrt/include/kernel-version.mk
+cp -f ./openwrt_release/include/toolchain-build.mk ./openwrt/include/toolchain-build.mk
+cp -f ./openwrt_release/include/kernel-defaults.mk ./openwrt/include/kernel-defaults.mk
+cp -f ./openwrt_release/package/base-files/image-config.in ./openwrt/package/base-files/image-config.in
+cp -f ./openwrt_release/version ./openwrt/version
+cp -f ./openwrt_release/version.date ./openwrt/version.date
+cp -rf ./openwrt_release/target/linux/* ./openwrt/target/linux/
+cp -rf ./openwrt_release/package/kernel/linux/* ./openwrt/package/kernel/linux/
+
+# 获取源代码
+#git clone -b main --depth 1 https://github.com/Lienol/openwrt.git openwrt-lienol
+#git clone -b main --depth 1 https://github.com/Lienol/openwrt-packages packages-lienol
+#git clone -b main --depth 1 https://github.com/Lienol/openwrt-luci luci-lienol
+#git clone -b linksys-ea6350v3-mastertrack --depth 1 https://github.com/NoTengoBattery/openwrt NoTengoBattery
+
+exit 0
---
+++ SCRIPTS/X86/02_target_only.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# 内核加解密组件
+echo '
+CONFIG_CRYPTO_AES_NI_INTEL=y
+' >>./target/linux/x86/config-5.10
+
+# UKSM
+#echo '
+#CONFIG_KSM=y
+#CONFIG_UKSM=y
+#' >>./target/linux/x86/64/config-5.4
+
+#Vermagic
+latest_version="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][0-9]/p' | sed -n 1p | sed 's/v//g' | sed 's/.tar.gz//g')"
+wget https://downloads.openwrt.org/releases/${latest_version}/targets/x86/64/packages/Packages.gz
+zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' >.vermagic
+sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
+
+# 预配置一些插件
+cp -rf ../PATCH/files ./files
+
+chmod -R 755 ./
+find ./ -name *.orig | xargs rm -f
+find ./ -name *.rej | xargs rm -f
+
+exit 0
