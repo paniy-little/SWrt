c259ade5a650607f110ba31d898ea0523607517f
yunshu <33997505+hello-yunshu@users.noreply.github.com>
2024-07-06 21:17:18 +0800

    .

--- SELF/02_prepare_package_self.sh
+++ SELF/02_prepare_package_self.sh
@@ -513,10 +513,10 @@ svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-eqos packa
 #luci-app-cloudflarespeedtest
 #svn export https://github.com/kenzok8/small-package/trunk/luci-app-cloudflarespeedtest package/new/luci-app-cloudflarespeedtest
 ##fix bug
-rm -rf ./package/kernel/mac80211/*
-cp -rf ../lede/package/kernel/mac80211/* ./package/kernel/mac80211/
-rm -rf ./package/kernel/ath10k-ct/*
-cp -rf ../lede/package/kernel/ath10k-ct/* ./package/kernel/ath10k-ct/
+#rm -rf ./package/kernel/mac80211/*
+#cp -rf ../lede/package/kernel/mac80211/* ./package/kernel/mac80211/
+#rm -rf ./package/kernel/ath10k-ct/*
+#cp -rf ../lede/package/kernel/ath10k-ct/* ./package/kernel/ath10k-ct/
 
 ### 最后的收尾工作 ###
 # Lets Fuck
