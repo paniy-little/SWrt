13e8bccfa9b6913a43e305e69f924e97473dcbf8
yunshu <louisowen@me.com>
2025-09-20 22:25:37 +0800

    Add odhcpd RFC 9096 compliance patch
    Introduces a patch to improve RFC 9096 compliance in odhcpd, allowing configuration of upper limits for preferred and valid lifetimes. Updates the package preparation script to use the new patch and disables fetching some older patches.

--- SELF/02_prepare_package_self.sh
+++ SELF/02_prepare_package_self.sh
@@ -218,11 +218,10 @@ wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Mak
 patch -p1 <../PATCH/pkgs/odhcp6c/1002-odhcp6c-support-dhcpv6-hotplug.patch
 # ODHCPD
 mkdir -p package/network/services/odhcpd/patches
-#cp -f ../PATCH/pkgs/odhcpd/001-odhcpd-improve-RFC-9096-compliance.patch ./package/network/services/odhcpd/patches/001-odhcpd-improve-RFC-9096-compliance.patch
-wget https://github.com/openwrt/odhcpd/pull/209.patch -O package/network/services/odhcpd/patches/209.patch
+cp -f ../PATCH/pkgs/odhcpd/001-odhcpd-improve-RFC-9096-compliance.patch ./package/network/services/odhcpd/patches/001-odhcpd-improve-RFC-9096-compliance.patch
+#wget https://github.com/openwrt/odhcpd/pull/211.patch -O package/network/services/odhcpd/patches/211.patch
 wget https://github.com/openwrt/odhcpd/pull/219.patch -O package/network/services/odhcpd/patches/219.patch
 wget https://github.com/openwrt/odhcpd/pull/242.patch -O package/network/services/odhcpd/patches/242.patch
-wget https://github.com/openwrt/odhcpd/pull/245.patch -O package/network/services/odhcpd/patches/242.patch
 mkdir -p package/network/ipv6/odhcp6c/patches
 wget https://github.com/openwrt/odhcp6c/pull/75.patch -O package/network/ipv6/odhcp6c/patches/75.patch
 wget https://github.com/openwrt/odhcp6c/pull/80.patch -O package/network/ipv6/odhcp6c/patches/80.patch
