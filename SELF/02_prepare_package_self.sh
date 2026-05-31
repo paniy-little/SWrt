128d8085e0d9b33671bd94a05703c01822002f7b
云云朮 <louisowen@me.com>
2026-06-01 07:22:42 +0800

    update: adapt to luci-app-upnp-bridge-relay package rename
    - 02_prepare_package_self.sh: package path → luci-app-upnp-bridge-relay
    - config_self.seed: CONFIG_PACKAGE_luci_app_upnp_bridge_relay=y

--- SELF/02_prepare_package_self.sh
+++ SELF/02_prepare_package_self.sh
@@ -234,7 +234,7 @@ cp -rf ../diskman/applications/luci-app-diskman ./package/new/luci-app-diskman
 mkdir -p package/new/parted && \
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/new/parted/Makefile
 # UPnP Bridge Relay
-cp -rf ../upnp-bridge-relay/package/upnp-bridge-relay ./package/new/upnp-bridge-relay
+cp -rf ../upnp-bridge-relay/package/luci-app-upnp-bridge-relay ./package/new/luci-app-upnp-bridge-relay
 # IPv6 兼容助手
 patch -p1 <../PATCH/pkgs/odhcp6c/1002-odhcp6c-support-dhcpv6-hotplug.patch
 # ODHCPD
